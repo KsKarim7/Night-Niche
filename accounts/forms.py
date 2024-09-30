671b914d933c1014dfbc9240710be4d0a14cb4ba
Khaled Karim <kskarim177@gmail.com>
2024-09-30 22:19:53 +0600

    User registration frontend

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,7 +8,7 @@ from django.core.validators import MinLengthValidator
 
 class UserRegistrationForm(UserCreationForm):
     birth_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
-    phone_number = forms.CharField(max_length=11, validators=[MinLengthValidator(11)], blank=True, null=True)
+    phone_number = forms.CharField(max_length=11, validators=[MinLengthValidator(11)])
   
     class Meta:
         model = User
@@ -29,3 +29,17 @@ class UserRegistrationForm(UserCreationForm):
                 account_no = 222010 + our_user.id
         )
         return our_user
+    
+    
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class' : (
+                    'appearance-none block w-full bg-blue-300 '
+                    'text-gray-700 border border-gray-200 rounded '
+                    'py-3 px-4 leading-tight focus:outline-none '
+                    'focus:bg-white focus:border-gray-500'
+                ) 
+            })
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from .forms import UserRegistrationForm
+from django.contrib.auth import login, logout,update_session_auth_hash
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
+from django.contrib.auth.forms import SetPasswordForm
 
 # Create your views here.
+
+class UserRegistrationView(FormView):
+    template_name = 'accounts/user_registration.html'
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self,form):
+        user = form.save() 
+        login(self.request, user)
+        return super().form_valid(form)
+    
